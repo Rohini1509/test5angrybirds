247e8aefa8e3487477b03022143cb621c64fea96
Rohini1509 <rohini.mdu85@gmail.com>
2020-08-05 15:25:44 +0530

    a new box is created

--- sketch.js
+++ sketch.js
@@ -30,6 +30,7 @@ function setup(){
     log3 =  new Log(810,180,300, PI/2);
 
     box5 = new Box(810,160,70,70);
+    box6 = new Box(300,200,50,50);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
